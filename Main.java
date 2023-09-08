1c178b9bb4c07ec901b8329f4758596eb562ac00
studentsamiul <144474053+studentsamiul@users.noreply.github.com>
2023-09-08 12:14:20 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+  public static void main(String[]args)
+  { 
+    System.out.println("Good Luck on the AP exam fellow tester");
+    System.out.println("Hopefully you will obtain a 5");
+    System.out.print("Through ease");
+  }
+}
